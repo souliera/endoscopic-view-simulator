f9d75eb18477bb31051f6b207f8dc322a5505583
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2018-10-16 23:14:22 +0200

    Adding basic files

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if(argc != 1) {
+        cout << "Usage: " << argv[0] << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // ****************************
+    // *** VARIABLE DECLARATION ***
+    // ****************************
+
+
+
+    // *******************************
+    // *** VARIABLE INITAILIZATION ***
+    // *******************************
+
+
+
+    // ***************
+    // *** PROGRAM ***
+    // ***************
+
+
+
+    // **************************
+    // *** VARIABLE RELEASING ***
+    // **************************
+
+    exit(EXIT_SUCCESS);
+}
